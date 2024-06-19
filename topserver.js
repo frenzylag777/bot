b8ace95644ca707208402c7438e72f42ce2bdae2
frenzylag777 <107318308+frenzylag777@users.noreply.github.com>
2024-06-19 18:35:16 +0300

    Update topserver.js

--- topserver.js
+++ topserver.js
@@ -31,6 +31,4 @@ bot.on("message", jsonMsg => {
   if (`${jsonMsg}`.includes("НАШ САЙТ:") === true)
     setTimeout(() => bot.chat("/bc НАШ САЙТ: https://tinyurl.com/topserversite"), 60000);
 })
-while (true) {
-  setTimeout(() => bot.chat("/tp ~ ~10 ~"), 10000);
-}
+setTimeout(() => bot.chat("/tp ~ ~10 ~"), 10000);
